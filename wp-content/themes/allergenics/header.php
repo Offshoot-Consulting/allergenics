93a3f743f832a57dcbabf76e639f913c9845cc89
Edyta Kulaga <edyta@aonach.com>
2015-07-23 11:54:08 +0200

    Google tag manager

--- wp-content/themes/allergenics/header.php
+++ wp-content/themes/allergenics/header.php
@@ -153,6 +153,17 @@ document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('on
 	</head>
 
 	<body <?php body_class(); ?>>
+	
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KDLF7T"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KDLF7T');</script>
+<!-- End Google Tag Manager -->
+	
 
         <div id="wrapper" <?php if ( is_page() || !is_active_sidebar( 'default-sidebar' )) echo 'class="full-width"'; ?>>
 
