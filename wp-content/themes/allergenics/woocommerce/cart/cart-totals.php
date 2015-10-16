91d300499c76e5eb418636eb23e30e4afd0f3f14
Edyta Kulaga <edyta@aonach.com>
2015-10-16 12:44:45 +0200

    New wording and layout of the urgnet box in cart

--- wp-content/themes/allergenics/woocommerce/cart/cart-totals.php
+++ wp-content/themes/allergenics/woocommerce/cart/cart-totals.php
@@ -65,20 +65,20 @@ else if(isset($_GET['removed_item']) && $_GET['removed_item'] == '1') {
   <?php if($how_many_tests_in_cart > 0) { ?>
     <div class="urgent-box">
       <?php if($how_many_tests_in_cart == 1) { ?>
-        <h3>Need quicker results?</h3>
-        <p>For just $20 per test you can have your tests processed urgently. <a class="tell-me-more" href="#modal-one">Tell me more</a></p>
-        <a href="/cart/?add-to-cart=574&variation_id=719&attribute_amount=1">Yes, process my test urgently</a>
+        <h3>Need your results faster?</h3>
+        <p>Add urgent processing to your order for just $20 per test, and get your results in just 3-5 working days. <a class="tell-me-more" href="#modal-one">Tell me more</a></p>
+        <a href="/cart/?add-to-cart=574&variation_id=719&attribute_amount=1">Yes please add urgent!</a>
       <?php } ?>
       
       <?php if($how_many_tests_in_cart > 1) { ?>
-        <h3>Need quicker results?</h3>
-        <p>For just $20 per test you can have your tests processed urgently. <a class="tell-me-more" href="#modal-one">Tell me more</a></p>
+        <h3>Need your results faster?</h3>
+        <p>Add urgent processing to your order for just $20 per test, and get your results in just 3-5 working days. <a class="tell-me-more" href="#modal-one">Tell me more</a></p>
       <?php } ?>
       
       <div class="modal" id="modal-one" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-header">
-            <h2>Need quicker results?</h2>
+            <h2>Need your results faster?</h2>
             <a href="#close" class="btn-close" aria-hidden="true">&#10006;</a> <!--CHANGED TO "#close"-->
           </div>
           <div class="modal-body">
@@ -91,15 +91,15 @@ else if(isset($_GET['removed_item']) && $_GET['removed_item'] == '1') {
         </div>
       
       <?php if($how_many_tests_in_cart == 2) { ?>
-        <a href="/cart/?add-to-cart=574&variation_id=720&attribute_amount=2">Yes, process my test urgently</a>
+        <a href="/cart/?add-to-cart=574&variation_id=720&attribute_amount=2">Yes please add urgent!</a>
       <?php } ?>
       
       <?php if($how_many_tests_in_cart == 3) { ?>
-        <a href="/cart/?add-to-cart=574&variation_id=721&attribute_amount=3">Yes, process my test urgently</a>
+        <a href="/cart/?add-to-cart=574&variation_id=721&attribute_amount=3">Yes please add urgent!</a>
       <?php } ?>
       
       <?php if($how_many_tests_in_cart == 4) { ?>
-        <a href="/cart/?add-to-cart=574&variation_id=722&attribute_amount=4">Yes, process my test urgently</a>
+        <a href="/cart/?add-to-cart=574&variation_id=722&attribute_amount=4">Yes please add urgent!</a>
       <?php } ?> 
     </div>
     <?php } // end if($items_in_cart > 0) ?>
